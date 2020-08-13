5cd5cc6bcf449b67a520f0cf667bff389e9fd80c
Samuel Bruin <67058024+Ancientkingg@users.noreply.github.com>
2020-08-13 17:52:58 +0200

    Reupload of all files

---
+++ main.js
@@ -0,0 +1,116 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+const { exec } = require('child_process');
+
+// SET ENV
+process.env.NODE_ENV = 'development';
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let addWindow;
+
+app.on('ready', function(){
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height:600
+  });
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'mainWindow.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+  mainWindow.on('closed', function(){
+    app.quit();
+  });
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+function createAddWindow(){
+  addWindow = new BrowserWindow({
+    width: 300,
+    height:200,
+    title:'PlaceholderName'
+  });
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes:true
+  }));
+  addWindow.on('close', function(){
+    addWindow = null;
+  });
+}
+
+ipcMain.on('item:add', function(e, item){
+  mainWindow.webContents.send('item:add', item);
+  addWindow.close(); 
+});
+
+// Create menu template
+const mainMenuTemplate =  [
+  {
+    label: 'File',
+    submenu:[
+      {
+        label:'Add Item',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label:'Placeholder',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
+      },
+      {
+        label: 'Quit',
+        accelerator:process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if(process.platform == 'darwin'){
+  mainMenuTemplate.unshift({});
+}
+
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        role: 'reload'
+      },
+      {
+        label: 'Toggle DevTools',
+        accelerator:process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        label: 'TestCMD',
+        click(item, focusedWindow){
+          exec('start tests.bat', (err, stdout, stderr) => {
+            if (err) {
+              //some err occurred
+              console.error(err)
+            } else {
+             // the *entire* stdout and stderr (buffered)
+             console.log(`stdout: ${stdout}`);
+             console.log(`stderr: ${stderr}`);
+            }
+          });
+        }
+      }
+    ]
+  });
+}
---
+++ test.bat
@@ -0,0 +1,3 @@
+@echo off
+dotnet DepotDownloader.dll -app 359550 -depot 377237 -manifest 1378283477131353042 -username <username> -password <password> -dir Downloads\Test -validate -max-servers 15 -max-downloads 10
+dotnet DepotDownloader.dll -app 359550 -depot 359551 -manifest 1739364586766771991 -username <username> -password <password> -dir Downloads\Test -validate -max-servers 15 -max-downloads 10
