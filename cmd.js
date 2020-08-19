716b1ad29a1a5373edd2dfbd48d0db4998137720
Ancientkingg <samuelmbruin@yahoo.com>
2020-08-19 19:58:33 +0200

    Update css

--- cmd.js
+++ cmd.js
@@ -1,36 +1,35 @@
 var is = require("electron-is");
 function setStatus(msg)    { getStatus().innerHTML = msg; };
-var x;
+var username;
+var password;
 
 
 function validateForm(event) {
   event.preventDefault()
-  x = document.forms["credentials"]["username"].value;
-  const data = new FormData(event.Target)
-  console.log(`Hello ${x}`)
-  if (x == "") {
+  username = document.forms["credentials"]["username"].value;
+  password = document.forms["credentials"]["password"].value;
+  startDownload()
+  if (username == "" || password == "") {
     return false;
   }
 }
 
-function TestCMD() {
-  console.log(x)
-}
 
 function startDownload() {
     const process = require('child_process');   // The power of Node.JS
 
-    var cmd = (is.windows()) ? 'dotnet DepotDownloader.dll -app 359550 -depot 377237 -manifest 299124516841461614 -username ancientkinggg -password WitBrood12#  -remember-password -dir "Downloads/test" -validate -max-servers 15 -max-downloads 10' : './test.sh';      
-    console.log('cmd:', cmd);
+    // var cmd = (is.windows()) ? `dotnet DepotDownloader.dll -app 359550 -depot 377237 -manifest 299124516841461614 -username ${username} -password ${password}  -remember-password -dir "Downloads/test" -validate -max-servers 15 -max-downloads 10` : './test.sh';      
+    var cmd = (is.windows()) ? `echo ${username}` : './test.sh';      
+    
+    // console.log('cmd:', cmd);
         
     var child = process.exec(cmd); 
 
 
     child.stdout.on('data', function (data) {
       console.log(JSON.stringify(data.toString()));
-      if(data.toString().startsWith("Unable to login to Steam3: RateLimitExceeded\r\n")){
-        console.log('say hi')
-        // child.stdin.write(prompt("gimme 2fa token noooow!"));
+      if(data.toString().startsWith("ancientkingg")){
+        console.log("Hey you're username is mine!")
       }
     });
 };
